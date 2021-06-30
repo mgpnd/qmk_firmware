9ee5c6dbc2f7498cee93b106d82269e870db4562
Artem Rashev <artem.rashev@protonmail.com>
2021-06-30 11:56:25 +0300

    Add factory and custom keymaps

---
+++ keyboards/lily58/keymaps/custom/rules.mk
@@ -0,0 +1,6 @@
+OLED_DRIVER_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+ENCODER_ENABLE = yes        # Enable encoder
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
---
+++ keyboards/lily58/keymaps/seller/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
+ENCODER_ENABLE = yes        # Enable encoder
+EXTRAKEY_ENABLE = yes
